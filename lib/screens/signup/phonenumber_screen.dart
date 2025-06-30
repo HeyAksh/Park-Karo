5bd41beb06df17ba2e0d18d224cd047d7b2474f0
HeyAksh <aksh07.me@gmail.com>
2025-07-01 01:04:39 +0530

    Created Account Setup Screen (input mobile number)

---
+++ lib/screens/signup/phonenumber_screen.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
+
+class PhoneNumberScreen extends StatelessWidget {
+  const PhoneNumberScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0x00000000),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 50.0,
+          right: 50.0,
+          bottom: 50.0,
+        ),
+        child: Column(
+          crossAxisAlignment:
+              CrossAxisAlignment.start,
+          mainAxisAlignment:
+              MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                const Text(
+                  'Enter Phone number for verification',
+                  style: TextStyle(
+                    color: Color(0xffffffff),
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.start,
+                ),
+
+                const SizedBox(height: 17.0),
+
+                const Text(
+                  '''This number will be used for all rides - related communication. You shall receive an SMS with code for verification.''',
+                  style: TextStyle(
+                    color: Color(0xffa1a1aa),
+                    fontSize: 13.5,
+                    fontWeight: FontWeight.normal,
+                  ),
+                  textAlign: TextAlign.start,
+                ),
+
+                const SizedBox(height: 107),
+
+                IntlPhoneField(
+                  keyboardType:
+                      TextInputType.phone,
+                  decoration:
+                      const InputDecoration(
+                        hintText: 'Mobile Number',
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                  initialCountryCode: 'IN',
+                  onChanged: (phone) {
+                    print(phone.completeNumber);
+                  },
+                  dropdownTextStyle: TextStyle(
+                    color: Colors.grey,
+                  ),
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                  dropdownIcon: const Icon(
+                    Icons.arrow_drop_down,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius:
+                    BorderRadius.circular(8.0),
+              ),
+              child: Center(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(
+                        vertical: 12.0,
+                      ),
+                  child: const Text(
+                    'Next',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
