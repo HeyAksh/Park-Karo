33b824dbda557e513c0b6ed377821f9f609433d4
HeyAksh <aksh07.me@gmail.com>
2025-07-01 01:01:36 +0530

    Created Splash Screen

---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import '../signup/signup_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() =>
+      _SplashScreenState();
+}
+
+class _SplashScreenState
+    extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 4), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder:
+              (context) => const SignUpScreen(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Row(
+          mainAxisAlignment:
+              MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Park ',
+              style: TextStyle(
+                color: Color(0xffffffff),
+                fontWeight: FontWeight.bold,
+                fontSize: 36,
+              ),
+            ),
+            const Text(
+              'Karo',
+              style: TextStyle(
+                color: Color(0xffe7fe54),
+                fontWeight: FontWeight.bold,
+                fontSize: 36,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
